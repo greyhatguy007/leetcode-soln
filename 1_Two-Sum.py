a258b14e06716ce73f9ef19aefd68b5fc5b87499
Ritvik <mritvikprasad@gmail.com>
2022-05-29 12:23:58 +0530

    added few solns

---
+++ 1_Two-Sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        length = len(nums)
+        for i in range(length):
+            diff = target - nums[i]         
+            if diff in nums[i+1:]: 
+                index = nums[i + 1:].index(diff)+i+1
+                return [i, index]
+            
