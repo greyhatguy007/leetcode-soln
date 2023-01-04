42507a036eb896f12b4bc9c32f2096d3558ca3a8
Ritvik <mritvikprasad@gmail.com>
2023-01-04 19:17:09 +0530

    Added optimised solution for 1572.java

--- 1572.java
+++ 1572.java
@@ -12,4 +12,15 @@ public class Leetcode1572 {
         }
         return ans;
     }
+
+    // Optimised Solution
+
+    public int diagonalSumOptimised(int[][] mat) {
+        int ans = 0;
+        for(int i=0;i<mat.length;i++){
+            ans += mat[i][i];
+            if(i!=mat.length-i-1)ans += mat[i][mat.length-i-1];
+        }
+        return ans;
+    }
 }
