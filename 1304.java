6e09144a185faa8b1413c297fc54a7f6d68150c2
Ritvik <mritvikprasad@gmail.com>
2023-01-07 12:28:07 +0530

    Added 1304.java

---
+++ 1304.java
@@ -0,0 +1,10 @@
+package com.learnDSA.leetcode;
+
+public class Leetcode1304 {
+    public int[] sumZero(int n) {
+        int[] A = new int[n];
+        for (int i = 0; i < n; ++i)
+            A[i] = i * 2 - n + 1;
+        return A;
+    }
+}
