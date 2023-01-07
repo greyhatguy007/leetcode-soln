849602c8581207587eed9dd93995c35465f011a4
Ritvik <mritvikprasad@gmail.com>
2023-01-07 13:01:14 +0530

    added 53.java

---
+++ 53.java
@@ -0,0 +1,33 @@
+package com.learnDSA.leetcode;
+
+public class Leetcode53 {
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+    }
+    public static int maxSubArray(int[] nums) {
+        int ans=0, temp=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i;j<nums.length;j++){
+                for(int k=i;k<=j;k++){
+                    temp+=nums[k];
+                }
+                if(ans<temp)ans=temp;
+                temp=0;
+            }
+
+        }
+        return ans;
+    }
+
+    // Optimised Solution
+
+    public int maxSubArrayOptimised(int[] nums){
+        int maxSoFar=nums[0], maxEndingHere=nums[0];
+        for (int i=1;i<nums.length;++i){
+            maxEndingHere= Math.max(maxEndingHere+nums[i],nums[i]);
+            maxSoFar=Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+}
