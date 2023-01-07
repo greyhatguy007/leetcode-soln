aa0835625671f9f9d66a16f1cb62aac84d00ac10
Ritvik <mritvikprasad@gmail.com>
2023-01-07 13:18:35 +0530

    Added 2022.java

---
+++ 2022.java
@@ -0,0 +1,11 @@
+package com.learnDSA.leetcode;
+
+public class Leetcode2022 {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if(original.length != m*n) return new int[0][0];
+        int[][] result = new int[m][n];
+        for(int i=0;i<original.length;i++)
+            result[i/n][i%n] = original[i];
+        return result;
+    }
+}
